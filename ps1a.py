6d5123b9430e0720d7eb4f4421a6f542f6413836
K-Pathfinder <madhavi1harish@gmail.com>
2026-02-18 18:51:25 +0530

    Update ps1a.py

--- ps1a.py
+++ ps1a.py
@@ -1,7 +1,15 @@
-a = int(input("Enter 1st number : "))
-b = int(input("Enter 2nd number : "))
-print(f"The sum of two numbers is {a + b}")
-print(f"The subtraction of two numbers is {a - b}")
-print(f"The product of two numbers is {a * b}")
-print(f"The division of two numbers is {a / b}")
-print(f"The modulus of two numbers is {a % b}")
+Annual_Salary=float(input("enter your annual salary : ")) 
+Saving_rate=float(input("enter the monthly saving rate in decimal : ")) 
+Total_cost=float(input("enter the cost of your dream house : ")) 
+Down_payment=0.25*Total_cost 
+Current_saving=0.0 
+Months=0
+Monthly_Salary=0 
+Annual_return=0.04 
+Monthly_return=Annual_return/12 
+while Down_payment>Current_saving: 
+    Monthly_Salary=Annual_Salary/12 
+    Current_saving+=Current_saving*Monthly_return 
+    Current_saving+=Monthly_Salary*Saving_rate 
+    Months+=1
+print("Number of months:",Months)
